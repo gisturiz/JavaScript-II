fe962c8c7ca7b8edde7bad9aeedfa8f763bde63d
Gustavo Isturiz <isturizgustavo@gmail.com>
2019-04-02 16:41:12 -0400

    callback project finish

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,49 +2,82 @@
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
+
 
   //Given this problem: 
   
-  function firstItem(arr, cb) {
-    // firstItem passes the first item of the given array to the callback function.
-  }
+  // function firstItem(arr, cb) {
+  //   // firstItem passes the first item of the given array to the callback function.
+  // }
 
-  // Potential Solution:
+  // // Potential Solution:
 
-  // Higher order function using "cb" as the call back
-  function firstItem(arr, cb) {
-    return cb(arr[0]);
-  }
+  // // Higher order function using "cb" as the call back
+  // function firstItem(arr, cb) {
+  //   return cb(arr[0]);
+  // }
 
-  // Function invocation 
-  firstItem(items, function(first) {
-    console.log(first)
-  });
+  // // Function invocation 
+  // firstItem(items, function(first) {
+  //   console.log(first)
+  // });
 
-*/
+
 
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
 }
 
+function getLength(arr, cb) {
+  return cb(arr.length);
+}
+
+getLength(items, function(length){
+  console.log(length)
+});
+
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
 }
 
+function last(arr, cb){
+  return cb(arr[arr.length - 1]);
+}
+
+last(items, function(lastItem){
+  console.log(lastItem)
+});
+
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x, y);
 }
+
+const add = function(x, y,) {
+  return x + y;
+}
+
+console.log(sumNums(2, 4, add));
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x, y);
 }
+
+const multiply = function(x, y,) {
+  return x * y;
+}
+
+console.log(sumNums(2, 4, multiply));
+
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
 }
+
+console.log(items.includes("Gum"));
 
 /* STRETCH PROBLEM */
 
